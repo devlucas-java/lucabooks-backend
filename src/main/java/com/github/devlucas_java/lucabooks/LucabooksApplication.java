ac59c54096baa943be7d1de1e28e1011bfcdda63
devlucas-java <1uc4sm4c3d00@gmail.com>
2025-08-26 21:28:05 +0200

    first commit

---
+++ src/main/java/com/github/devlucas_java/lucabooks/LucabooksApplication.java
@@ -0,0 +1,13 @@
+package com.github.devlucas_java.lucabooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucabooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucabooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/devlucas_java/lucabooks/LucabooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.devlucas_java.lucabooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucabooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
